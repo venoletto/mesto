5007d601eafd89511c36a620cbc00d4632ed0863
Kamil Gataullin <fdaw.qxxz@gmail.com>
2022-06-03 13:43:09 +0300

    <type> (feat) first commit

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+let closeButton = document.querySelector('.popup__close-button');
+let openButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let name = document.querySelector('.profile__text');
+let bio = document.querySelector('.popup__username-input');
+let status = document.querySelector('.profile__subtext');
+let statusInfo = document.querySelector('.popup__status-input');
+let formElement = document.querySelector('.popup__form')
+
+function openPopup () {
+  popup.classList.remove('popup__remove');
+  bio.value = name.textContent;
+  statusInfo.value = status.textContent;
+
+}
+
+function closePopup() {
+    popup.classList.add('popup__remove');
+}
+
+openButton.addEventListener('click', openPopup);
+
+closeButton.addEventListener('click', closePopup);
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    name.textContent = bio.value;
+    status.textContent = statusInfo.value;
+    closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
