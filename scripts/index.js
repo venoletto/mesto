2ac88f8e7b52dc2686f690f530055a41da40bfe7
Kamil Gataullin <fdaw.qxxz@gmail.com>
2022-06-06 18:53:40 +0300

    fix: ещё исправления

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,20 @@ let closeButton = document.querySelector('.popup__close-button');
 let openButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let name = document.querySelector('.profile__text');
-let bio = document.querySelector('.popup__username-input');
+let bio = document.querySelector('.popup__input_username');
 let status = document.querySelector('.profile__subtext');
-let statusInfo = document.querySelector('.popup__status-input');
+let statusInfo = document.querySelector('.popup__input_status');
 let formElement = document.querySelector('.popup__form')
 
 function openPopup () {
-  popup.classList.remove('popup_remove');
+  popup.classList.remove('popup__remove');
   bio.value = name.textContent;
   statusInfo.value = status.textContent;
 
 }
 
 function closePopup() {
-    popup.classList.add('popup_remove');
+    popup.classList.add('popup__remove');
 }
 
 openButton.addEventListener('click', openPopup);
