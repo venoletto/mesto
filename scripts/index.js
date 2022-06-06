77b20e6284a949e1363fd116ece6ac8436c82595
Kamil Gataullin <fdaw.qxxz@gmail.com>
2022-06-06 19:27:55 +0300

    fix: некоторые изменения с js

--- scripts/index.js
+++ scripts/index.js
@@ -8,14 +8,14 @@ let statusInfo = document.querySelector('.popup__input_status');
 let formElement = document.querySelector('.popup__form')
 
 function openPopup () {
-  popup.classList.remove('popup__remove');
+  popup.classList.remove('popup');
   bio.value = name.textContent;
   statusInfo.value = status.textContent;
 
 }
 
 function closePopup() {
-    popup.classList.add('popup__remove');
+    popup.classList.add('popup');
 }
 
 openButton.addEventListener('click', openPopup);
