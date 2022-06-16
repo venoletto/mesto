fd080e2c5945f9d7cb91a784a3b7caf1f6f47b57
Kamil Gataullin <fdaw.qxxz@gmail.com>
2022-06-16 19:25:32 +0300

    feat: поменял логику размещения карточек и добавил новый попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,110 @@
-let closeButton = document.querySelector('.popup__close-button');
-let openButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
+let closeButton = document.querySelectorAll('.popup__close-button');
+let popupEdit = document.querySelector('.popup__edit');
+let popupAdd = document.querySelector('.popup__add');
+let openEditButton = document.querySelector('.profile__edit-button');
+let openAddButton = document.querySelector('.profile__add-button');
 let name = document.querySelector('.profile__text');
-let bio = document.querySelector('.popup__input_place_username');
 let status = document.querySelector('.profile__subtext');
+let formElementAdd = document.querySelector('.popup__form-add');
+let formElementEdit = document.querySelector('.popup__form-edit');
+let bio = document.querySelector('.popup__input_place_username');
 let statusInfo = document.querySelector('.popup__input_place_status');
-let formElement = document.querySelector('.popup__form')
+let popLand = document.querySelector('.popup__input_place_land');
+let popLink = document.querySelector('.popup__input_place_link');
 
-function openPopup () {
-  popup.classList.add('popup_opened');
+function openPopup (i) {
+  i.classList.add('popup_opened')
+}
+
+function closePopup (i) {
+  i.classList.remove('popup_opened');
+}
+
+function popupEditOpen(){
+  openPopup(popupEdit)
   bio.value = name.textContent;
   statusInfo.value = status.textContent;
-
 }
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
+openEditButton.addEventListener('click', popupEditOpen);
 
-openButton.addEventListener('click', openPopup);
+openAddButton.addEventListener('click', function(){openPopup(popupAdd)});
 
-closeButton.addEventListener('click', closePopup);
+closeButton.forEach(function(e) {
+  e.addEventListener('click', function(close) {
+      const actualPopup = close.target.closest('.popup');
+      closePopup(actualPopup);
+  });
+});
 
-
-function formSubmitHandler (evt) {
+function editSubmitHandler (evt) {
     evt.preventDefault();
     name.textContent = bio.value;
     status.textContent = statusInfo.value;
-    closePopup();
+    closePopup(popupEdit);
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+function addSubmitHandler (evt) {
+  evt.preventDefault();
+  closePopup(popupAdd);
+}
 
+formElementEdit.addEventListener('submit', editSubmitHandler);
+
+formElementAdd.addEventListener('submit', addSubmitHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+let elementsTemplate = document.querySelector('.elements__template').content;
+let elements = document.querySelector('.elements');
+
+function startElements (cell){
+  let newElement = elementsTemplate.querySelector('.elements__cell').cloneNode(true);
+  newElement.querySelector('.elements__name').textContent = cell.name;
+  newElement.querySelector('.elements__image').src = cell.link;
+  elements.insertAdjacentElement('afterbegin', newElement);
+
+  return newElement;
+}
+
+initialCards.forEach(startElements);
