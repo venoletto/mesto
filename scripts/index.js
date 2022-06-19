ca32504e862a3733cbf4216ff990257900cd0ed1
Kamil Gataullin <fdaw.qxxz@gmail.com>
2022-06-19 23:06:53 +0300

    fix: небольшие изменения в функциях

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
-let closeButton = document.querySelectorAll('.popup__close-button');
-let popupEdit = document.querySelector('.popup_type_edit');
-let popupAdd = document.querySelector('.popup_type_add');
-let openEditButton = document.querySelector('.profile__edit-button');
-let openAddButton = document.querySelector('.profile__add-button');
-let name = document.querySelector('.profile__text');
-let status = document.querySelector('.profile__subtext');
-let formElementAdd = document.querySelector('.popup__form-add');
-let formElementEdit = document.querySelector('.popup__form-edit');
-let bio = document.querySelector('.popup__input_place_username');
-let statusInfo = document.querySelector('.popup__input_place_status');
-let popLand = document.querySelector('.popup__input_place_land');
-let popLink = document.querySelector('.popup__input_place_link');
-let picPopup = document.querySelector('.popup_type_opened-picture');
+const closeButton = document.querySelectorAll('.popup__close-button');
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupAdd = document.querySelector('.popup_type_add');
+const openEditButton = document.querySelector('.profile__edit-button');
+const openAddButton = document.querySelector('.profile__add-button');
+const name = document.querySelector('.profile__text');
+const status = document.querySelector('.profile__subtext');
+const formElementAdd = document.querySelector('.popup__form-add');
+const formElementEdit = document.querySelector('.popup__form-edit');
+const bio = document.querySelector('.popup__input_place_username');
+const statusInfo = document.querySelector('.popup__input_place_status');
+const popLand = document.querySelector('.popup__input_place_land');
+const popLink = document.querySelector('.popup__input_place_link');
+const picPopup = document.querySelector('.popup_type_opened-picture');
+const elementsTemplate = document.querySelector('.elements__template').content;
+const elements = document.querySelector('.elements');
+const popImage = picPopup.querySelector('.popup__image');
+const popFigcaption = picPopup.querySelector('.popup__figcaption');
 
 function openPopup (i) {
   i.classList.add('popup_opened')
@@ -27,6 +31,57 @@ function popupEditOpen(){
   statusInfo.value = status.textContent;
 }
 
+function editSubmitHandler (evt) {
+    evt.preventDefault();
+    name.textContent = bio.value;
+    status.textContent = statusInfo.value;
+    closePopup(popupEdit);
+}
+
+function addSubmitHandler (evt) {
+  evt.preventDefault();
+  const addElement = {name, link,}
+  addElement.name = popLand.value;
+  addElement.link = popLink.value;
+  renderCard(addElement);
+  closePopup(popupAdd);
+  formElementAdd.reset();
+}
+
+function createNewElements (cell){
+  const newElement = elementsTemplate.querySelector('.elements__cell').cloneNode(true);
+  const elementPic = newElement.querySelector('.elements__image');
+  const elementText = newElement.querySelector('.elements__name');
+  elementText.textContent = cell.name;
+  elementPic.src = cell.link;
+  elementPic.alt = cell.name;
+
+  const trashButton = newElement.querySelector('.elements__trash');
+  trashButton.addEventListener('click', function(){
+    newElement.remove();
+  })
+
+  const likeButton = newElement.querySelector('.elements__like-button');
+  function pushLike(){
+    likeButton.classList.toggle('elements__like-button_type_active')
+  }
+  likeButton.addEventListener('click', pushLike);
+  
+  elementPic.addEventListener('click', function picPopupOpened () {
+    popImage.src = elementPic.src;
+    popFigcaption.textContent = elementText.textContent;
+    openPopup(picPopup);
+  })
+  
+  return newElement;
+}
+
+function renderCard(newElement) {
+  elements.insertAdjacentElement('afterbegin', createNewElements(newElement));
+}
+
+initialCards.forEach(renderCard);
+
 openEditButton.addEventListener('click', popupEditOpen);
 
 openAddButton.addEventListener('click', function(){openPopup(popupAdd)});
@@ -38,100 +93,6 @@ closeButton.forEach(function(e) {
   });
 });
 
-function editSubmitHandler (evt) {
-    evt.preventDefault();
-    name.textContent = bio.value;
-    status.textContent = statusInfo.value;
-    closePopup(popupEdit);
-}
-
-function addSubmitHandler (evt) {
-  evt.preventDefault();
-  let addElement = {name, link,}
-  addElement.name = popLand.value;
-  addElement.link = popLink.value;
-  newElements(addElement);
-  closePopup(popupAdd);
-  formElementAdd.reset();
-}
-
 formElementEdit.addEventListener('submit', editSubmitHandler);
 
 formElementAdd.addEventListener('submit', addSubmitHandler);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-let elementsTemplate = document.querySelector('.elements__template').content;
-let elements = document.querySelector('.elements');
-
-function newElements (cell){
-  let newElement = elementsTemplate.querySelector('.elements__cell').cloneNode(true);
-  let elementPic = newElement.querySelector('.elements__image');
-  let elementText = newElement.querySelector('.elements__name');
-  elementText.textContent = cell.name;
-  elementPic.src = cell.link;
-  elements.insertAdjacentElement('afterbegin', newElement);
-
-
-  let trashButton = document.querySelector('.elements__trash');
-  trashButton.addEventListener('click', function(){
-    newElement.remove();
-  })
-
-  let likeButton = newElement.querySelector('.elements__like-button')
-  likeButton.addEventListener('click', function(){
-    likeButton.classList.toggle('elements__like-button_type_active')
-  })
-  
-  elementPic.addEventListener('click', function picPopupOpened () {
-    let popImage = picPopup.querySelector('.popup__image');
-    let popFigcaption = picPopup.querySelector('.popup__figcaption');
-    popImage.src = elementPic.src;
-    popFigcaption.textContent = elementText.textContent;
-    openPopup(picPopup);
-  })
-  
-  return newElement;
-}
-
-initialCards.forEach(newElements);
